b7806a9cbe3c007adf8590081865619768b4536f
mixiancmx <83197963+mixiancmx@users.noreply.github.com>
2022-01-14 12:21:57 +0800

    Update dataloader.py

--- loader/dataloader.py
+++ loader/dataloader.py
@@ -31,10 +31,51 @@ class day_set(torch_data.Dataset):
         self.input=input
         self.output=output
     def __getitem__(self,index):
-        x=torch.from_numpy(self.input[index].astype(np.float)).type(torch.float)
+
+
+        x=self.input[index].astype(np.float)
+        # if x[:4].any()==1:
+        #     for i in range(9*4,16*4):
+        #         x[i][4]=1
+
+
+        #x=torch.from_numpy(self.input[index].astype(np.float)).type(torch.float)
+        x=torch.from_numpy(x).type(torch.float)
         
         y=torch.from_numpy(self.output[index].astype(np.float)).type(torch.float)
 
         return x,y
     def __len__(self):
         return self.input.shape[0] 
+
+class day_info_set(torch_data.Dataset):
+    def __init__(self, input, output) -> None:
+        super().__init__()
+        self.input=input
+        self.output=output
+    def __getitem__(self,index):
+
+
+        x=self.input[index,:,[4,5,6,7,8]].astype(np.float)
+        # if x[:4].any()==1:
+        #     for i in range(9*4,16*4):
+        #         x[i][4]=1
+        info=self.input[index,0,[0,9,10]].tolist()
+
+
+        #x=torch.from_numpy(self.input[index].astype(np.float)).type(torch.float)
+        x[:,4]=x[:,4]*100
+        x=torch.from_numpy(x).type(torch.float)
+        
+        y=torch.from_numpy(self.output[index].astype(np.float)).type(torch.float)
+
+        return x,y,info
+    def __len__(self):
+        return self.input.shape[0] 
+
+if __name__ == '__main__':
+    input=np.load('./data/input_18month_no_nan.npy',allow_pickle=True)
+
+    output=np.load('./data/output_18month_no_nan.npy',allow_pickle=True)
+    aset=day_set(input,output)
+    print(aset[1])
